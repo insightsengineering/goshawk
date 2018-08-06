f67a7291237539d09930d759b0c4ac40ca1f0bdb
Paszty, Nick {MDBT~South San Francisco} <paszty.nicholas@gene.com>
2018-08-06 11:11:16 -0700

    Add files via upload

---
+++ goshawk_function_template.R
@@ -0,0 +1,35 @@
+#' Function to generate a ...
+#' Output render by teal.goshawk module
+#'
+#' \code{function_name} returns descriptive summary statistics table
+#'
+#' @param add as many param lines as there are params that this function expects
+#' @param add as many param lines as there are params that this function expects
+#'
+#' @author Balazs Toth
+#' @author first last
+#'
+#' @details provide additional information as needed. perhaps link to specification file
+#'
+#' @return \code{returned object name} object
+#'
+#' @export
+#'
+#' @template author_unixid
+#'
+#' @examples
+#'
+#'
+
+# library includes
+
+#g/t_function_name(Bali's function parameters here, ...){
+
+  # check input arguments first for validation/error trapping
+  # if (any("- Overall -" %in% term))
+  #  stop("'- Overall -' is not a valid term, function reserves it for derivation")
+
+  # Bali's function code here
+
+
+#}
