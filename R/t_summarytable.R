67dab00978fe7ea2ba2d04d5d9d4a1c71bf6b1f0
Paszty, Nick {MDBT~South San Francisco} <paszty.nicholas@gene.com>
2018-09-11 01:13:53 +0200

    First commit of density distirbution and summary table

--- R/t_summarytable.R
+++ R/t_summarytable.R
@@ -5,10 +5,12 @@
 #' descriptive summary statistics table
 #'
 #' @param data frame name
-#' @param biomarker PARAM value
 #' @param trt_group treatment group variable name e.g. ARM
-#' @param time visit variable name e.g. VISIT
-#' @param loq loq variable name e.g. loq_flag (not in current ALB)
+#' @param param_var name of variable containing biomarker codes e.g. PARAMCD.
+#' @param param biomarker to visualize e.g. IGG. 
+#' @param xaxis_var name of variable containing biomarker results displayed on X-axis e.g. BASE.
+#' @param visit_var name of variable containing visit values e.g. AVISITCD
+#' @param loq_flag_var loq variable name e.g. loq_flag_var (not in current ALB)
 #'
 #' @author Balazs Toth
 #' @author Nick Paszty
@@ -22,45 +24,43 @@
 #' @examples
 #'
 #' ALB <- read_bce("/opt/bee/home_nas/npaszty/btk/lupus/dataadam/alb3arm.sas7bdat")
-#' biomarker <- c('IGG') # FOR TESTING: woud come from teal.goshawk.tm_g_moduleName.R
+#' param <- c('IGG') # FOR TESTING: woud come from teal.goshawk.tm_g_moduleName.R
 #' 
 #' # call function
 #' t_summarytable(data = ALB,
 #'                trt_group = 'ARM',
-#'                biomarker_var = 'PARAMCD',
-#'                biomarker = biomarker, # the PARAMCD value
-#'                value_var = 'AVAL',
-#'                visit = 'AVISIT',
-#'                loq = NULL)
-#'               
+#'                param_var = 'PARAMCD',
+#'                param = param, # the PARAMCD value
+#'                xaxis_var = 'AVAL',
+#'                visit_var = 'AVISITCD',
+#'                font_size = 12,
+#'                loq_flag_var = 'LOQFL')
+#'
+#' table1
 #'
 t_summarytable <- function(data,
                            trt_group,
-                           biomarker_var,
-                           biomarker,
-                           value_var,
-                           visit,
-                           loq, ...){
+                           param_var,
+                           param,
+                           xaxis_var,
+                           visit_var = 'AVISITCD',
+                           font_size = 12,
+                           loq_flag_var = 'LOQFL', ...){
   
-  # Helper
-  sum_data <- data %>%
-    filter(eval(parse(text = biomarker_var)) == biomarker) %>%
-    group_by(eval(parse(text = visit)),
-             eval(parse(text = trt_group))) %>%
-    summarise(n = sum(!is.na(eval(parse(text = value_var)))),
-              avg = round(mean(eval(parse(text = value_var)),
-                               na.rm = TRUE),1),
-              med = round(median(eval(parse(text = value_var)),
-                                 na.rm = TRUE),1),
-              sd = round(sd(eval(parse(text = value_var)),
-                            na.rm = TRUE),1)
-              #pctLOQ = round(100 * sum(eval(parse(text = loq))=='YES')/
-              #                 length(eval(parse(text = loq))=='YES'),2)
+  table_data <- data %>%
+    filter(eval(parse(text = param_var)) == param)
+  
+  # by ARM table
+  sum_data_by_arm <- table_data %>%
+    filter(eval(parse(text = param_var)) == param) %>%
+    group_by_(.dots = c(param_var, trt_group, visit_var)) %>%
+    summarise(n = sum(!is.na(eval(parse(text = xaxis_var)))),
+              Mean = round(mean(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              Median = round(median(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              StdDev = round(sd(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              Min = round(min(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              Max = round(max(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              PctMiss = round(100 * sum(is.na(eval(parse(text = xaxis_var))))/length(is.na(eval(parse(text = xaxis_var)))),1),
+              PctLOQ =  round(100 * sum(eval(parse(text = loq_flag_var))=='Y')/length(eval(parse(text = loq_flag_var))=='Y'),2)
               )
-
-  tbl <- tableGrob(t(sum_data))
-  grid.arrange(arrangeGrob(tbl, ncol=1, nrow=1))
-
-  return(tbl)
-
 }
