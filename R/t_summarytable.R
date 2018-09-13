e0b8e364a9cf9b87018893fbf357d54525a73aad
Paszty, Nick {MDBT~South San Francisco} <paszty.nicholas@gene.com>
2018-09-13 16:52:23 +0200

    Update the tsummarytable function only

--- R/t_summarytable.R
+++ R/t_summarytable.R
@@ -2,7 +2,7 @@
 #' plots.
 #' 
 #' Output render by teal.goshawk module \code{t_summarytable} returns
-#' descriptive summary statistics table
+#' descriptive summary statistics table as a data frame
 #'
 #' @param data frame name
 #' @param trt_group treatment group variable name e.g. ARM
@@ -23,8 +23,8 @@
 #'
 #' @examples
 #'
-#' ALB <- read_bce("/opt/bee/home_nas/npaszty/btk/lupus/dataadam/alb3arm.sas7bdat")
-#' param <- c('IGG') # FOR TESTING: woud come from teal.goshawk.tm_g_moduleName.R
+#'\dontrun{
+#' # use post processed data created in app.R file
 #' 
 #' # call function
 #' t_summarytable(data = ALB,
@@ -36,8 +36,9 @@
 #'                font_size = 12,
 #'                loq_flag_var = 'LOQFL')
 #'
-#' table1
+#'}
 #'
+
 t_summarytable <- function(data,
                            trt_group,
                            param_var,
@@ -47,11 +48,11 @@ t_summarytable <- function(data,
                            font_size = 12,
                            loq_flag_var = 'LOQFL', ...){
   
-  table_data <- data %>%
+  table_data <<- data %>%
     filter(eval(parse(text = param_var)) == param)
   
   # by ARM table
-  sum_data_by_arm <- table_data %>%
+  sum_data_by_arm <<- table_data %>%
     filter(eval(parse(text = param_var)) == param) %>%
     group_by_(.dots = c(param_var, trt_group, visit_var)) %>%
     summarise(n = sum(!is.na(eval(parse(text = xaxis_var)))),
@@ -62,5 +63,33 @@ t_summarytable <- function(data,
               Max = round(max(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
               PctMiss = round(100 * sum(is.na(eval(parse(text = xaxis_var))))/length(is.na(eval(parse(text = xaxis_var)))),1),
               PctLOQ =  round(100 * sum(eval(parse(text = loq_flag_var))=='Y')/length(eval(parse(text = loq_flag_var))=='Y'),2)
-              )
+              ) %>%
+        # not sure why using trt_group instead of ARM doesn't work so using ARM
+        # !!!!!!!!!! THIS CODE IS NOT GENERIC SO NEEDS TO BE UPDATED !!!!!!!!!!
+        mutate(row_ord = ifelse(grepl("1", ARM), 1, ifelse(grepl("2", ARM), 2, ifelse(grepl("3", ARM), 3, NA))))
+
+  # by combined ARM table
+  sum_data_combined_arm <<- table_data %>%
+    filter(eval(parse(text = param_var)) == param) %>%
+    group_by_(.dots = c(param_var, visit_var)) %>%
+    summarise(n = sum(!is.na(eval(parse(text = xaxis_var)))),
+              Mean = round(mean(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              Median = round(median(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              StdDev = round(sd(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              Min = round(min(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              Max = round(max(eval(parse(text = xaxis_var)), na.rm = TRUE),1),
+              PctMiss = round(100 * sum(is.na(eval(parse(text = xaxis_var))))/length(is.na(eval(parse(text = xaxis_var)))),1),
+              PctLOQ =  round(100 * sum(eval(parse(text = loq_flag_var)) == 'Y')/length(eval(parse(text = loq_flag_var)) == 'Y'),2)
+    ) %>%
+    mutate(ARM = "Comb.", row_ord = 4)
+  
+  # combine the two data sets and apply some formatting
+  sum_data <<- rbind(sum_data_by_arm, sum_data_combined_arm) %>% # concatenate
+    arrange(PARAMCD, AVISITCD, row_ord) %>% # sort
+    mutate(Biomarker = PARAMCD, Treatment = ARM, Visit = AVISITCD) %>% # rename variables
+    ungroup() %>% # need to ungroup to drop previously identified grouping variables PARAMCD and ARM
+    select(-PARAMCD, -ARM, -AVISITCD, -row_ord) %>% # drop
+    select(Biomarker, Treatment, Visit, n:PctLOQ) # reorder variables
+    
+  
 }
