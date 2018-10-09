17cc2128757e2676116a2bc89667c06006b1b457
luiw2 <wenyi.liu@roche.com>
2018-10-09 16:01:54 +0200

    added font_size argument for lineplot and spaghetti plot functions

--- R/g_lineplot.R
+++ R/g_lineplot.R
@@ -17,6 +17,7 @@
 #' @param median boolean whether to display median results.
 #' @param hline numeric value represnting intercept of horizontal line.
 #' @param roate_xlab boolean whether to rotate x-axis labels.
+#' @param font_size control font size for title, x-axis, y-axis and legend font.
 #' 
 #' @import ggplot2
 #' @import dplyr
@@ -36,84 +37,9 @@
 #' @export
 #'
 #' @examples
-#' # EXAMPLE 1:
 #' 
-#' library(dplyr)
-#' library(ggplot2)
-#' library(gridExtra)
-#' library(grid)
-#' library(stringr)
-#' library(DescTools)
+#' # EXAMPLE:
 #' 
-#' # for development team testing
-#' ASL_path <- "~/btk/lupus/dataadam/asl.sas7bdat"
-#' ALB_path <- "~/btk/lupus/dataadam/alb3arm.sas7bdat"
-#' 
-#' # list of biomarkers of interest. see ALB2 assignment below
-#' param_choices <- c("CRP","ADIGG","IG","IGA","IGE","IGG","IGM","TEST")
-#' 
-#' ASL0 <- read_bce(ASL_path)
-#' ASL <- subset(ASL0, subset = ITTFL == 'Y' & IAFL == 'Y')
-#' 
-#' ALB0 <- read_bce(ALB_path)
-#' 
-#' # post process the data to subset records per specification
-#' ALB_SUBSET <- subset(ALB0,
-#'                      subset = PARAMCD %in% c(param_choices) & ITTFL == 'Y' & IAFL == 'Y' & ANLFL == 'Y' & AVISIT %like any% c('BASE%','%WEEK%'),
-#'                      select = c('STUDYID', 'USUBJID', 'ITTFL', 'ANLFL', 'ARM', 'AVISIT', 'AVISITN', 'PARAMCD', 'AVAL', 'AVALU', 'BASE', 'CHG', 'PCHG',
-#'                                 'LBSTRESC', 'LBSTRESN'))
-#' 
-#' # calculate the minimum AVAL for each PARAMCD
-#' PARAM_MINS <- ALB_SUBSET %>%
-#'   select(USUBJID, PARAMCD, AVAL) %>%
-#'   filter(PARAMCD %in% param_choices) %>%
-#'   group_by(PARAMCD) %>%
-#'   summarise(AVAL_MIN=min(AVAL, na.rm=TRUE))
-#' 
-#' # post process the data to create several new variables and adjust existing record specific valules per specification
-#' # - create a visit code variable - baseline record code is "BB" and week records coded to "W NN"
-#' # - adjust existing BASELINE record values where values are missing: According to SPA this is a STREAM artifact
-#' ALB_SUPED1 <- ALB_SUBSET %>% mutate(AVISITCD = paste0(substr(AVISIT,start=1, stop=1),
-#'                                                       substr(AVISIT, start=regexpr(" ", AVISIT), stop=regexpr(" ", AVISIT)+2))) %>%
-#'   mutate(AVISITCD = ifelse(AVISITCD == "BB", "BL", AVISITCD)) %>%
-#'   mutate(AVISITCDN =  ifelse(AVISITCD == "BL", 0, substr(AVISITCD,start=2, stop=4))) %>%
-#'   mutate(BASE = ifelse(AVISIT == "BASELINE" & is.na(BASE), AVAL, BASE)) %>%
-#'   mutate(CHG = ifelse(AVISIT == "BASELINE" & is.na(CHG), 0, CHG)) %>%
-#'   mutate(PCHG = ifelse(AVISIT == "BASELINE" & is.na(PCHG), 0, PCHG))
-#' # may need to add similar code for BASE2 related variables
-#' 
-#' 
-#' # merge minimum AVAL value onto the ALB data to calculate the log2 variables and preserve the variable order
-#' ALB_SUPED2 <- merge(ALB_SUPED1, PARAM_MINS, by="PARAMCD")[, union(names(ALB_SUPED1), names(PARAM_MINS))] %>%
-#'   mutate(AVALL2 = ifelse(AVAL == 0, log2(AVAL_MIN/2), log2(AVAL))) %>%
-#'   mutate(BASEL2 = ifelse(BASE == 0, log2(AVAL_MIN/2), log2(BASE))) #%>% need SPA to finish adding BASE2 to ALB
-#' #mutate(BASE2L2 = ifelse(BASE2 == 0, log2(AVAL_MIN/2), log2(AVAL)))
-#' 
-#' # for proper chronological ordering of visits in visualizations
-#' ALB_SUPED2$AVISITCDN <- as.numeric(ALB_SUPED2$AVISITCDN) # coerce character into numeric
-#' ALB <- ALB_SUPED2 %>% mutate(AVISITCD = factor(AVISITCD) %>% reorder(AVISITCDN))
-#' 
-#' # to test loq_flag_var
-#' ALB <- ALB %>% mutate(LOQFL = ifelse(PARAMCD == "CRP" & AVAL < .5, "Y", "N"))
-#' 
-#' param <- c('CRP') # FOR TESTING: woud come from teal.goshawk.tm_g_moduleName.R
-#' shape_manual <- c('N' = 1, 'Y' = 2, 'NA' = 0)
-#' 
-#' plot1 <- g_lineplot(label = 'Line Plot',
-#'                     data = ALB,
-#'                     biomarker_var = 'PARAMCD',
-#'                     biomarker = 'CRP',
-#'                     value_var = 'AVAL',
-#'                     trt_group = 'ARM',
-#'                     time = 'AVISITCD',
-#'                     color_manual = NULL,
-#'                     median = FALSE,
-#'                     hline = NULL,
-#'                     rotate_xlab = FALSE)
-#' plot1
-#' 
-#' # EXAMPLE 2:
-#'
 #' library(dplyr)
 #' library(ggplot2)
 #' library(gridExtra)
@@ -146,7 +72,7 @@
 #'            median = FALSE,
 #'            hline = NULL,
 #'            rotate_xlab = FALSE)
-#'            
+
 
 
 g_lineplot <- function(label = 'Line Plot',
@@ -163,7 +89,8 @@ g_lineplot <- function(label = 'Line Plot',
                        color_manual = NULL,
                        median = FALSE,
                        hline = NULL,
-                       rotate_xlab = FALSE) {
+                       rotate_xlab = FALSE,
+                       font_size = 12) {
   
   ## Pre-process data
   if(!is.null(trt_group_level)){
@@ -232,7 +159,7 @@ g_lineplot <- function(label = 'Line Plot',
     xlab(time) + 
     ylab(paste0(biomarker, ' ', line, title))+
     theme(legend.position = "bottom",
-          plot.title = element_text(size=18, margin = margin()),
+          plot.title = element_text(size=font_size, margin = margin()),
           axis.title.y = element_text(margin = margin(r = 20)))
     
 
@@ -253,6 +180,17 @@ g_lineplot <- function(label = 'Line Plot',
     plot1 <- plot1 +
       geom_hline(aes(yintercept = hline), color="red", linetype="dashed", size=0.5)
   }
+  
+  # Format font size
+  if (!is.null(font_size)){
+    plot1 <- plot1 +
+      theme(axis.title.x = element_text(size = font_size),
+            axis.text.x = element_text(size = font_size),
+            axis.title.y = element_text(size = font_size),
+            axis.text.y = element_text(size = font_size),
+            legend.title = element_text(size = font_size),
+            legend.text = element_text(size = font_size))
+  }
 
   
   ## number of obs table
@@ -272,7 +210,8 @@ g_lineplot <- function(label = 'Line Plot',
           axis.ticks =  element_blank(),
           axis.title.x=element_blank(),
           axis.title.y = element_blank(),
-          plot.title = element_text(size=12))
+          axis.text.y = element_text(size=font_size),
+          plot.title = element_text(face = "bold", size=font_size))
   
   glist <- lapply(list(plot=plot1, text=tbl), ggplotGrob)
   leftmar <- do.call(unit.pmax, lapply(glist, "[[", "widths"))
