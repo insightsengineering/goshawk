c5e35e7cdd6f8b9f0285e7468b3ed820a6ef2196
Paszty, Nick {MDBT~South San Francisco} <paszty.nicholas@gene.com>
2019-07-22 23:23:18 +0200

    deleted file. dplyr related notes will not be quieted in goshawek or teal.goshawk. even with the notes the package build is NEST worthy.

--- R/global_variables.R
+++
@@ -1,8 +0,0 @@
-#' To prevent "no visible binding for global variable" note during R CMD check.
-#' 
-#' @name global_variables
-
-utils::globalVariables(c("intercept", "slope", "corr",
-                         ".", "str_count",
-                         "hcl",
-                         "PctLOQ", "TRTORD", ":=", "MAXTRTORDVIS", "Biomarker", "Visit"))
