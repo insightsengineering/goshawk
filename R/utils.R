6e88d5e6be4b61acdcd8f93ac513e6a25058001e
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-06-28 11:03:21 +0100

    fix h_caption_logs_label

--- R/utils.R
+++ R/utils.R
@@ -84,26 +84,14 @@ h_identify_loq_values <- function(loqs_data) {
 h_caption_loqs_label <- function(loqs_data) {
   loq_values <- h_identify_loq_values(loqs_data)
 
-  if (is.na(loq_values$LLOQC)) {
-    lloq_value <- "NA"
-  } else {
-    lloq_value <- loq_values$LLOQC
-  }
-
-  if (is.na(loq_values$ULOQC)) {
-    uloq_value <- "NA"
-  } else {
-    uloq_value <- loq_values$ULOQC
-  }
-
   # create caption
   caption_loqs_label <- paste0(
     "Limits of quantification read from study data for ",
     loqs_data$PARAM,
     ": LLOQ is ",
-    lloq_value,
+    loq_values$LLOQC,
     ", ULOQ is ",
-    uloq_value
+    loq_values$ULOQC
   )
 
   return(caption_loqs_label)
