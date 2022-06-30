8134866949be801e907d89d2a18a2eaeb51a6ca4
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-06-30 16:32:03 +0100

    use NA string

--- R/utils.R
+++ R/utils.R
@@ -84,6 +84,9 @@ h_identify_loq_values <- function(loqs_data) {
 h_caption_loqs_label <- function(loqs_data) {
   loq_values <- h_identify_loq_values(loqs_data)
 
+  loq_values$LLOQC <- ifelse(is.na(loq_values$LLOQC), "NA", loq_values$LLOQC)
+  loq_values$ULOQC <- ifelse(is.na(loq_values$ULOQC), "NA", loq_values$ULOQC)
+
   # create caption
   caption_loqs_label <- paste0(
     "Limits of quantification read from study data for ",
