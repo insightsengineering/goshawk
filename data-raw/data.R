d609a2b8d72e795c78f68de50b3e4e5d7e23f061
m7pr <marcin.kosinski.mk1@roche.com>
2023-07-11 12:48:46 +0200

    add #nolint to data-raw/data.R

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,6 +1,6 @@
-# library(scda)
-# rADLB <- synthetic_cdisc_data("latest")$adlb
-# usethis::use_data(rADLB)
-#
-# rADSL <- synthetic_cdisc_data("latest")$adsl
-# usethis::use_data(rADSL)
+library(scda)
+rADLB <- synthetic_cdisc_data("latest")$adlb #nolint
+usethis::use_data(rADLB)
+
+rADSL <- synthetic_cdisc_data("latest")$adsl #nolint
+usethis::use_data(rADSL)
